274e3f390c462eb3e6c25def3c3b409aba89fccb
hushino <frostshock@outlook.com>
2019-01-30 10:47:31 -0300

    Initial commit

---
+++ src/main/java/com/red/springboot/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.red.springboot.repositories;
+
+import com.red.springboot.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByName(String name);
+}
